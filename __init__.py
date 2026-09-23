f6056ae2c2a6a2459aafad157ccbeb69f92623b0
Roberto Geraldes <37715015+Lyghtnyng@users.noreply.github.com>
2026-09-23 16:45:10 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2026 Roberto Geraldes <rob.filipe007@hotmail.com>
-# Road Centerline Extractor - QGIS Plugin
-# Licensed under GNU GPL v2 or later
-def classFactory(iface):
-    from .plugin import RoadCenterlinePlugin
-    return RoadCenterlinePlugin(iface)
